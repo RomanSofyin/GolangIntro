44dad525139421863f92a1e2928f7a432150ec27
Roman Sofin <roma.sofin@gmail.com>
2020-04-18 16:42:29 +0300

    Add error demo

---
+++ week2/error_demo/error_demo.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(a int, b int) int {
+	return a / b
+}
+
+func main() {
+
+	var input int
+	_, err := fmt.Scan(&input) // функция Scan возвращает два параметра, но нам сейчас важно проверить только ошибку
+	if err != nil {
+		fmt.Println("Проверьте типы входных параметров")
+	} else {
+		fmt.Println(divide(input, 5)) //Выведем результат, если ошибок нет
+	}
+
+	var usrErr = errors.New("my error")
+	fmt.Println("", usrErr)
+}
